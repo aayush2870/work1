c6ebd553d44dc63f1301aeaa66d841d32cccca34
aayush2870 <aayushraj2881@gmail.com>
2024-01-03 15:18:18 +0530

    updated styles.css
    added goto top button

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ gsap.to("#nav", {
 });
 
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topBtn").style.display = "block";
+  } else {
+    document.getElementById("topBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
